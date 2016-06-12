281e2183d9aa8bd77996893aed41a1923b02c75d
Bernardo Pacheco <bernardo.wpacheco@gmail.com>
2016-06-12 00:11:19 -0300

    chore(project): first commit

---
+++ src/file-uploader.directive.ts
@@ -0,0 +1,7 @@
+import { Directive, ElementRef } from '@angular/core';
+@Directive({ selector: '[myHighlight]' })
+export class HighlightDirective {
+    constructor(el: ElementRef) {
+       el.nativeElement.style.backgroundColor = 'yellow';
+    }
+}
